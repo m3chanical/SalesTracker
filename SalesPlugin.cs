e5d7822f04e00c7dc71815afbcf866bbdfaac4cf
sinbeard <etc@example.com>
2021-01-31 21:29:22 -0500

    make more gooder

--- SalesPlugin.cs
+++ SalesPlugin.cs
@@ -7,27 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SalesTracker
 {
     public class SalesPlugin : BotPlugin
     {
-        private static int sale_count = 0;
-        private static int gil = 0;
-        private Regex gil_regex = new Regex(@"\b(\d*,*\d*,*\d{1,3})\b", RegexOptions.Compiled); // matches any number from 1 to 999,999,999 w/ commas
+        private static int sale_count;
+        private static int gil;
+        private readonly Regex gil_regex = new Regex(@"The (\d*) (.*) you put up for sale in the (.*) markets have sold for (\d*,*\d*,*\d*) gil .*", RegexOptions.Compiled); 
 
         public override string Author => "Sinbeard";
 
         public override string Name => "Market Board Sales Tracker";
 
-        public override Version Version => new Version(0, 1, 0);
+        public override Version Version => new Version(1, 2, 0);
         public override bool WantButton => true;
         public override string ButtonText => "Log Report";
 
         public override void OnButtonPress() 
         {
             // output chat log with sales
-            Logger.Info($"You have made {sale_count} sales and {gil} gil.");
+            report_gil();
+
+            try
+            {
+                var form = new SettingsForm();
+                form.Show();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
         }
 
         public override void OnInitialize()
@@ -38,6 +49,11 @@ namespace SalesTracker
         public override void OnEnabled()
         {
             GamelogManager.MessageRecevied += GamelogManager_MessageRecevied;
+            // load settings? 
+        }
+        public override void OnDisabled() 
+        {
+            GamelogManager.MessageRecevied -= GamelogManager_MessageRecevied;
         }
 
         private void GamelogManager_MessageRecevied(object sender, ChatEventArgs e) 
@@ -46,17 +62,21 @@ namespace SalesTracker
             {
                 case MessageType.RetainerSaleReports:
                     sale_count++;
-                    Logger.Info("You made a sale! Wow. Cool.");
+                    Logger.Info("Market Board sale made.");
                     Logger.Verbose($"Cool Sales Message: " + e.ChatLogEntry.Contents);
 
-                    var groups = gil_regex.Match(e.ChatLogEntry.Contents).Groups;
-                    if (groups.Count != 0) {
+                    var matches = gil_regex.Match(e.ChatLogEntry.Contents);
+                    if (matches.Success)
+                    {
+                        var groups = matches.Groups;
                         var amount = int.Parse(groups[0].ToString().Replace(",", ""));
                         Logger.Verbose($"Received {amount:n0} from the sale. Adding to total...");
 
                         gil += amount;
 
                         Logger.Info($"You have made {sale_count} sales, and {gil:n0} since starting the bot.");
+
+                        report_gil();
                     }
                     break;
                 case MessageType.Tell_Receive:
@@ -67,9 +87,12 @@ namespace SalesTracker
             }      
         }
 
-        public override void OnDisabled() 
+        private void report_gil() 
         {
-            GamelogManager.MessageRecevied -= GamelogManager_MessageRecevied;
+            Logger.Info("*~*~*~*~*~*~*~*~*~*~");
+            Logger.Info($"Items sold: {sale_count:n0}");
+            Logger.Info($"Gil obtained: {gil:n0}");
+            Logger.Info("*~*~*~*~*~*~*~*~*~*~");
         }
 
     }
