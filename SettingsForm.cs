59dc6bfbf7349966dee790fefb0d5975fbfe9a48
sinbeard <etc@example.com>
2022-01-16 00:56:44 -0500

    actually fix the thing. also remove redundant statements

--- SettingsForm.cs
+++ SettingsForm.cs
@@ -84,7 +84,7 @@ namespace SalesTracker
                     salesRange = _saleRange; // when dates are selected
                 }
                 
-                if (!SalesSettings.Instance.Sales.Any()) // e.g. new install or cleared stats
+                if (!salesRange.Any()) // e.g. new install or cleared stats
                     return false;
 
                 TimeSpan timeElapsed = salesRange.Last().SalesDateTime.Subtract(salesRange.First().SalesDateTime);
@@ -99,14 +99,12 @@ namespace SalesTracker
 
                 totalSoldLabel.Text = itemSum.ToString();
                 gilLabel.Text = gilSum.ToString();
-                if(itemSum > 0) gilPerItemLabel.Text = (gilSum / itemSum).ToString();
-                if(SalesSettings.Instance.Sales.Count > 0 ) gilPerSaleLabel.Text = (gilSum / salesRange.Count).ToString();
-                
+
                 totalSoldLabel.Text = $"{itemSum}";
                 gilLabel.Text = $"{gilSum:n0}";
-                if (itemSum != 0)
+                if (itemSum > 0)
                     gilPerItemLabel.Text = $"{gilSum / (itemSum):n0}"; 
-                gilPerSaleLabel.Text = $"{gilSum / SalesSettings.Instance.Sales.Count:n0}";
+                gilPerSaleLabel.Text = $"{gilSum / salesRange.Count:n0}";
                 
                 if (timeElapsed.TotalDays > 0) gilPerDayLabel.Text = timeElapsed.TotalDays < 1 ? $"{gilSum:n0}" : $"{gilSum / timeElapsed.TotalDays:n0}";
                 if (timeElapsed.TotalHours > 0) gilPerHourLabel.Text = $"{gilSum / timeElapsed.TotalHours:n0}";
